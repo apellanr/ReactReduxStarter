e99a7a5e5938f9596a2cf983fb4cb6b6bf531424
apellanr <ryan.apellanes@gmail.com>
2017-06-09 14:53:08 -0700

    fetching data from API request

--- src/index.js
+++ src/index.js
@@ -1,16 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 import Searchbar from './components/search_bar';
+import VideoList from './components/video_list';
 
 const API_KEY = 'AIzaSyBXbUnmIGk89-wbpZxX9P3OtjAszxJRXTI';
 
 // Create a new component. This component should produce some HTML
-const App = () => {
-    return (
-        <div>
-            <Searchbar />
-        </div>
-    );
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { videos: [] };
+
+        YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+            this.setState({ videos });
+            // this.setState({videos})
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <Searchbar />
+                <VideoList videos={this.state.videos} />
+            </div>
+        );
+    }
 }
 
 // take this component's generated HTML and put it
