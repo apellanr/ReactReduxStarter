367d9fdc28193186a7d39889129ce768452a4ce7
apellanr <ryan.apellanes@gmail.com>
2017-06-09 01:55:19 -0700

    search component setup with event handler

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,2 +1,14 @@
-import React from 'react';
+import React, {Component} from 'react';
+
+class SearchBar extends Component {
+    render() {
+        return <input onChange={(event) => console.log('input change: ', event.target.value)}/>;
+    }
+
+    handleInputChange(event) {
+        console.log('input change: ', event.target.value);
+    }
+}
+
+export default SearchBar;
 
--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Searchbar from './components/search_bar';
 
 const API_KEY = 'AIzaSyBXbUnmIGk89-wbpZxX9P3OtjAszxJRXTI';
 
 // Create a new component. This component should produce some HTML
 const App = () => {
-    return <div>Hi!</div>;
+    return (
+        <div>
+            <Searchbar />
+        </div>
+    );
 }
 
 // take this component's generated HTML and put it
 // on the page (in the DOM)
-ReactDOM.render(<App/>, document.querySelector('.container'));
+ReactDOM.render(
+    <App/>, 
+    document.querySelector('.container')
+);
