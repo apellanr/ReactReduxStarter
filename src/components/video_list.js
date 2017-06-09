9331dd2363e02c16b90e7032adf5f06eabbed8b0
apellanr <ryan.apellanes@gmail.com>
2017-06-09 15:12:28 -0700

    video list functional component added

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import VideoListItem from './video_list_item';
 
 const VideoList = (props) => {
     return (
         <ul className="col-md-4 list-group">
-            {props.videos.length}
+            
         </ul>
     )
 }
 
 export default VideoList;
+
+// should loop over video array and render out one element
