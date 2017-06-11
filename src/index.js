2e93675400c72bdb6a8896639bbac7b8c4df09f1
apellanr <ryan.apellanes@gmail.com>
2017-06-11 02:14:36 -0700

    debounce with lodash added to project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
@@ -17,19 +18,24 @@ class App extends Component {
             selectedVideo: null
         };
 
-        YTSearch({key: API_KEY, term: 'french bulldog puppies'}, (videos) => {
+        this.videoSearch('game of thrones');
+    }
+
+    videoSearch(term) {
+        YTSearch({key: API_KEY, term: term}, (videos) => {
             this.setState({ 
                 videos: videos,
                 selectedVideo: videos[0] 
             });
-            // this.setState({videos})
         });
     }
 
     render() {
+        const videoSearch = _.debounce((term) => { this.videoSearch(term) }, 300 );
+
         return (
             <div>
-                <Searchbar />
+                <Searchbar onSearchTermChange={videoSearch} />
                 <VideoDetail video={this.state.selectedVideo} />
                 <VideoList 
                     onVideoSelect={selectedVideo => this.setState({selectedVideo})}
