16d8bf5027913143cc81cc398c762410aa82f59b
apellanr <ryan.apellanes@gmail.com>
2017-07-05 22:39:37 -0700

    form prevent default

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -13,9 +13,15 @@ export default class Searchbar extends Component {
         this.setState({term: event.target.value})
     }
 
+    onFormSubmit(event) {
+        event.preventDefault();
+
+        // we need to go and fetch weather data
+    }
+
     render() {
         return (
-            <form className="input-group">
+            <form onSubmit={this.onFormSubmit} className="input-group">
                 <input 
                     placeholder="5-day forecast in your favorite cities"
                     className="form-control"
