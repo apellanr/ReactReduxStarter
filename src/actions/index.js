ab8391d861baa85eef115759f4c168341fd2804d
apellanr <ryan.apellanes@gmail.com>
2017-07-09 23:52:15 -0700

    further understanding of promises and payloads

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,16 +1,18 @@
 import axios from 'axios';
 
 const API_KEY = '2922d50aaa6701b1d26a9260344b76ef';
-const ROUTE_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
 export function fetchWeather(city) {
     const url = `${ROOT_URL}&q=${city},us`;
-    const request = axios.get(url);
+    const request = axios.get(url); // promise does not contain any data
+
+    console.log('Request:', request);
 
     return {
         type: 'FETCH_WEATHER',
-        payload: request
+        payload: request // returning the promise as the payload
     };
 }
---
+++ src/reducers/reducer_weather.js
@@ -0,0 +1,5 @@
+// reducers are just functions
+export default function(state = null, action) {
+    console.log('Action received', action);
+    return state;
+}
